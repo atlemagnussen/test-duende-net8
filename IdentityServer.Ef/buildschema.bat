48811017813f32e590a460e0648a7317acd5bd45
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-18 00:20:54 +0200

    tested with sql server and ef

---
+++ IdentityServer.Ef/buildschema.bat
@@ -0,0 +1,11 @@
+rmdir /S /Q Migrations
+
+dotnet ef migrations add Grants -c PersistedGrantDbContext -o Migrations/PersistedGrantDb
+dotnet ef migrations add Configuration -c ConfigurationDbContext -o Migrations/ConfigurationDb
+
+dotnet ef migrations script -c PersistedGrantDbContext -o Migrations/PersistedGrantDb.sql
+dotnet ef migrations script -c ConfigurationDbContext -o Migrations/ConfigurationDb.sql
+
+
+@REM  dotnet ef database update -c PersistedGrantDbContext
+@REM dotnet ef database update -c ConfigurationDbContext
