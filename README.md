4995d9f862b62aa4468cc0ff68c7635024169855
Atle Magnussen <atlemagnussen@gmail.com>
2024-06-17 18:42:51 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Duende Identity Server .NET8
+
+https://duendesoftware.com/products/identityserver
---
+++ Web/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestDuende.Web.Controllers
+{
+    public record KeyVal
+    {
+        public string? Key { get; init; }
+        public string? Value { get; init; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public List<KeyVal> List()
+        {
+            var values = new List<KeyVal>
+            {
+                new KeyVal { Key = "1", Value = "value"}
+            };
+
+            return values;
+        }
+    }
+}
